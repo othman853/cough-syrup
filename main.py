9e0d4b07dc0d287ff5f89787d1380eb4f3205822
othman853 <anuar.yasser@gmail.com>
2016-08-04 18:28:12 -0300

    Main file is running app inside try/except block

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ from app import factory
 
 
 if __name__ == '__main__':
-    app = factory('etc.config.DevelopmentConfig')
-    app.run()
+
+    try:
+        app = factory('etc.config.DevelopmentConfig')
+        app.run()
+    except Exception as error:
+        print(str(error))
