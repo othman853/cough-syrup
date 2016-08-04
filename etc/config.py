83dc66ef92a8a282404c1bb742e0a93f3e949f6b
othman853 <anuar.yasser@gmail.com>
2016-08-04 18:27:49 -0300

    Safe function to read configs from environment

--- etc/config.py
+++ etc/config.py
@@ -3,11 +3,28 @@ import os
 
 TEST_BROWSER = os.environ.get('TEST_BROWSER', 'chrome')
 
+
+def from_env(env_var):
+    value = os.environ.get(env_var)
+
+    if value is None:
+        raise Exception(
+            '[CONFIG ERROR]: {} environment variable is either'
+            ' invalid or not set'
+            .format(env_var)
+        )
+
+    return value
+
+
 class DefaultConfig():
     TESTING = False
+    SQLALCHEMY_DATABASE_URI = from_env('SYRUP_DATABASE_URL')
+
 
 class DevelopmentConfig(DefaultConfig):
     DEBUG = True
+
 
 class TestConfig(DefaultConfig):
     TESTING = True
