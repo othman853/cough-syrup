88cd292d51c3d79798ce9a7f10a28dc265b929d4
othman853 <anuar.yasser@gmail.com>
2016-08-04 18:29:07 -0300

    Adding some models and integrating app with flask security

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask_security import Security, SQLAlchemyUserDatastore
+from .models import db
+from .models.auth import User, Role
+
 
 def factory(config):
 
     app = Flask(__name__)
 
-    if config:
-        app.config.from_object(config)
+    config = config or 'etc.config.DefaultConfig'
+    app.config.from_object(config)
+
+    auth_datastore = SQLAlchemyUserDatastore(db, User, Role)
+    security = Security(app, auth_datastore)
 
     return app
