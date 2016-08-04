07060c35e2b23e91233f72f5bdff7de3446e9e3d
othman853 <anuar.yasser@gmail.com>
2016-08-04 14:22:05 -0300

    Fixing venv script params

--- bin/venv.sh
+++ bin/venv.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
-SCRIPT_ROOT=$0
-VENV_NAME=$1
+SCRIPT_ROOT=$1
+VENV_NAME=$2
 
 if [ ! -d "$SCRIPT_ROOT/$VENV_NAME" ]; then
     pip3 install virtualenv
     mkdir $VENV_NAME
     python -m venv $VENV_NAME
+    $VENV_NAME/bin/pip3 install --upgrade pip
+    $VENV_NAME/bin/pip3 install --upgrade setuptools
 else
     echo 'Virtualenv already created. Run: "source .venv/bin/activate" to use it'
 fi
