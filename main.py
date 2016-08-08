e757832bb8227c3b41ddb8f1a4c42d7649eb407b
othman853 <anuar.yasser@gmail.com>
2016-08-08 17:06:27 -0300

    Creating and registering first pluggable view

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from app import factory
+from app.views.users.list import UserListView
+from app.models import db
 import os
 
 if __name__ == '__main__':
 
     try:
-        app = factory(os.env.get('SYRUP_CONFIG', 'DevelopmentConfig'))
+        app = factory(os.environ.get('SYRUP_CONFIG', 'DevelopmentConfig'))
+
+        app.add_url_rule(
+            '/users',
+            view_func=UserListView.as_view('users.index')
+        )
+
         app.run()
     except Exception as error:
         print(str(error))
